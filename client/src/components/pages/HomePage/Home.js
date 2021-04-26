74e0ff9a13bcba6f5628f6b3ce4540770aea8561
Anirudh Arora <anirudharora19@gmail.com>
2021-04-26 14:40:56 +0530

    Notifications Done

--- client/src/components/pages/HomePage/Home.js
+++ client/src/components/pages/HomePage/Home.js
@@ -3,8 +3,11 @@ import HeroSection from '../../HeroSection';
 import Navbar from '../../Navbar/Navbar';
 import Footer from '../Footer.js/Footer';
 import { homeObjOne, homeObjFour } from './Data';
+import firebase from '../../../firbase'
 
 function Home() {
+
+  
   return (
     <>
       <Navbar/>
