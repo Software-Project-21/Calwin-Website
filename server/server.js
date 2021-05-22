082efd0b5e8b98474c0a0514b85c01a848b5c900
Ranveer251 <sidana.manik251@gmail.com>
2021-05-22 12:56:19 +0530

    .

--- server/server.js
+++ server/server.js
@@ -23,9 +23,9 @@ app.use(express.json());
     })
   );
 
-// if(process.env.NODE_ENV==='production'){
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-// }
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
 // app.use("/",authMiddleware);
 app.use("/api/invite",invitesRoute);
@@ -36,12 +36,12 @@ app.use("/api/invite",invitesRoute);
 //   console.log("aa gaye yaha");
 // });
 
-// if(process.env.NODE_ENV === 'production')
-// {
-//   app.get("*", (req, res) => { 
-//     res.sendFile(path.join(__dirname , "../client/build/index.html"));
-//   });
-// }
+if(process.env.NODE_ENV === 'production')
+{
+  app.get("*", (req, res) => { 
+    res.sendFile(path.join(__dirname , "../client/build/index.html"));
+  });
+}
 
 app.listen(port, function(){
     console.log(`Server started at port ${port}`);
